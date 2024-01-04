34376ff093ce6ae715ec4a7715d5f16ff95b8752
Andreas Gottwald <andreas.gottwald@hotmail.de>
2024-01-04 21:15:46 +0100

    finish

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,13 +1,27 @@
+import styled from "styled-components";
+
 export default function Card() {
   return (
-    <div className="card">
-      <h3 className="card__title">Homer Simpson</h3>
-      <p className="card__description">
+    <CardWrapper>
+      <CardTitle> Homer Simpson</CardTitle>
+      <CardDescription>
         Homer Jay Simpson is a fictional character and the main protagonist of
         the American animated sitcom The Simpsons. He is voiced by Dan
         Castellaneta and first appeared, along with the rest of his family, in
         The Tracey Ullman Show short Good Night on April 19, 1987.
-      </p>
-    </div>
+      </CardDescription>
+    </CardWrapper>
   );
 }
+
+const CardWrapper = styled.div`
+  background-color: crimson;
+`;
+
+const CardTitle = styled.h3`
+  color: green;
+`;
+
+const CardDescription = styled.p`
+  color: blue;
+`;
